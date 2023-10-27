29a60ccd04d2dde99012400862d7fa5d773cde2e
José Melgar <melgarpuertasjose@gmail.com>
2023-10-27 16:16:54 -0500

    SCRUM-9 Realice los cambios necesarios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import logo from '../logo.svg';
+import { Link } from 'react-router-dom';
 
 const Footer = ()=>{
     return (
@@ -10,7 +11,9 @@ const Footer = ()=>{
                     <div>Derechos reservados ©</div>
                     <div>4Healthy <span className="text-primary">❤</span></div>
                 </div>
-                <div className="block text-right"><a href="#">Términos y condiciones</a></div>
+                <div className="block text-right">
+                <Link to="/Terminos">Términos y condiciones</Link>
+                </div>
             </div>
         </footer>
     );
