bafa815ae3c0f9b45349452bb997ad9440a3b220
José Melgar <melgarpuertasjose@gmail.com>
2023-10-27 16:01:12 -0500

    SCRUM-9 <Realice mis cambios>

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Signin from './pages/Signin';
 import Welcome from './pages/Welcome';
-// BrowserRouter
+import Restore from './pages/Restore';
+import Terminos from './pages/Terminos';
 // import Example from './pages/Example';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/" element={<Restore/>}/>
+        <Route path="/Signin" element={<Signin/>}/>
         <Route path="/welcome" element={<Welcome/>}/>
+        <Route path="/Terminos" element={<Terminos/>}/>
       </Routes>
-      {/* <Example/> */}
     </Router>
   );
 }
