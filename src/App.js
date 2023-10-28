40353064fbbaf8c1f6ff465b925ccaf7b1f73411
Jhamil Brijan Peña Cárdenas <111939233+Jaed69@users.noreply.github.com>
2023-10-28 01:03:47 -0500

    SCRUM-13 Login creacion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from './pages/Signup';
-import Signin from './pages/Signin';
+import Login from './pages/Login';
 import Welcome from './pages/Welcome';
 // BrowserRouter
 // import Example from './pages/Example';
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Signup/>}/>
-        <Route path="/singin" element={<Signin/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/welcome" element={<Welcome/>}/>
       </Routes>
       {/* <Example/> */}
