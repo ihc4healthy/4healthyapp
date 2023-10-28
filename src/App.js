0f440db8d3fabbf8aab7aaa1ecd1413ceb6e7454
José Melgar <melgarpuertasjose@gmail.com>
2023-10-28 01:54:35 -0500

    Modifiqué el Restore

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Logros from './pages/Logros';
 import Restore from './pages/Restore';
 import Terminos from './pages/Terminos';
 // BrowserRouter
-import Restore from './pages/Restore';
-import Terminos from './pages/Terminos';
 import Verification from './pages/Verification'
 import Confirm from './pages/Confirm'
 // import Example from './pages/Example';
@@ -30,10 +28,7 @@ function App() {
         <Route path="/logros" element={<Logros/>}/>
         <Route path="/terminos" element={<Terminos/>}/>
         <Route path="/restore" element={<Restore/>}/>
-        <Route path="/" element={<Restore/>}/>
         <Route path="/Signin" element={<Signin/>}/>
-        <Route path="/welcome" element={<Welcome/>}/>
-        <Route path="/Terminos" element={<Terminos/>}/>
         <Route path="/Verification" element={<Verification/>}/>
         <Route path="/Confirm" element={<Confirm/>}/>
       </Routes>
