f3410ac0edfcc952ff04d6f67f609e4472c2c830
Coralain <carolain.antoc@gmail.com>
2023-11-11 10:50:01 -0500

    SCRUM-25 separating avatars>

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import welcome from '../svgs/welcome.svg';
 import { AcademicCapIcon, BriefcaseIcon, CheckIcon, NewspaperIcon } from '@heroicons/react/24/solid'
 import ToggleButton from '../components/ToggleButton';
+import { avatars } from '../common/avatars';
 
 function Welcome() {
     const [persona, setPersona] = useState("");
@@ -29,19 +30,15 @@ function Welcome() {
                 <div className='flex flex-col gap-y-4'>
                     <p>Quiero usar 4Healty para organizar mis hábitos saludables y...</p>
                     <ToggleButton buttons={[
-                        {icon: <BriefcaseIcon/>, text:"trabajo", value:"trabajo"},
-                        {icon: <AcademicCapIcon/>, text:"estudios", value:"estudios"},
-                        {icon: <NewspaperIcon/>, text:"ambos", value:"ambos"},
+                        {icon: <BriefcaseIcon/>, text:"trabajo", value:"WORK"},
+                        {icon: <AcademicCapIcon/>, text:"estudios", value:"STUDIES"},
+                        {icon: <NewspaperIcon/>, text:"ambos", value:"ANY"},
                     ]} setChoosed={setPersona}/>
                 </div>
                 
                 <div className='flex flex-col gap-y-4'>
                     <p>¡Elige un avatar para tu perfil!</p>
-                    <ToggleButton buttons={[
-                        {imageSrc: "avatars/avatar_female.png", value:"avatar1", text:"avatar with long hair"},
-                        {imageSrc: "avatars/avatar_inter.png", value:"avatar2", text:"avatar with medium hair"},
-                        {imageSrc: "avatars/avatar_male.png", value:"avatar3", text:"avatar with short hair"},
-                    ]} defaultSelected={1} setChoosed={setAvatar}/>
+                    <ToggleButton buttons={avatars} defaultSelected={1} setChoosed={setAvatar}/>
                 </div>
 
                 <button className='btn-primary w-full' type='submit' disabled={persona==""}>
