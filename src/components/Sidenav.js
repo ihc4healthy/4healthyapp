f639ff568b42c95e7a8091b40a766e757ed6b02d
Coralain <carolain.antoc@gmail.com>
2023-11-12 01:20:55 -0500

    SCRUM-49 user stats summary

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {Card,Typography,List,ListItem,ListItemPrefix,ListItemSuffix,Chip} from "@material-tailwind/react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import {Card,List,ListItem,ListItemPrefix,ListItemSuffix,Chip, Progress, Tooltip} from "@material-tailwind/react";
 import {
   PresentationChartBarIcon,
   ShoppingBagIcon,
@@ -8,13 +8,13 @@ import {
   Cog6ToothIcon,
   InboxIcon,
   PowerIcon,
+  HeartIcon,
 } from "@heroicons/react/24/solid";
 import Header from "./Header";
 import Footer from "./Footer";
-import { avatars } from "../common/avatars";
+import UserStats from './UserStats';
 
 export const DSidebar = ()=> {
-  const avatar = avatars[1];
   const tabs = [
     {
       icon: <PresentationChartBarIcon className="h-5 w-5" />,
@@ -22,7 +22,7 @@ export const DSidebar = ()=> {
     },
     {
       icon: <ShoppingBagIcon className="h-5 w-5" />,
-      text: "MIS HÁBITOS", url: "#",
+      text: "MIS HÁBITOS", url: "/habits",
     },
     {
       icon: <InboxIcon className="h-5 w-5" />,
@@ -42,25 +42,24 @@ export const DSidebar = ()=> {
     },
   ];
   
-  const [selected, setSelected] = React.useState(1);
-  const setSelectedItem = (value) => setSelected(value);
+  const location = useLocation();
+  const getFirstRoute = (path) => path?.split('/')[1];
 
+  const [user, setUser] = useState({id: -1, name: "Usuario", email:"", level:1, levelProgress:10, avatar:1});
+  // const [selected, setSelected] = React.useState(1);
+  // const setSelectedItem = (value) => setSelected(value);
+  
   return (
     <Card className="sticky top-0 h-[calc(100vh-1rem)] w-full max-w-[20rem] p-4
                     flex flex-col shadow-xl shadow-blue-gray-900/5">
       <Header/>
 
-      <div className="flex py-4 items-center">
-        <img src={`images/${avatar.imageSrc}`} alt={avatar.text} className="h-40"/>
-        <Typography variant="h5" color="blue-gray" className="center">
-          Usuario
-        </Typography>
-      </div>
+      <UserStats user={user}/>
 
       <List className="font-heading">
         {tabs.map((tab, i) =>
         <Link href={tab.url} className="text-text-primary" key={`li-${i}`}>
-          <ListItem onClick={tab.onClick} selected={selected === i}>
+          <ListItem onClick={tab.onClick} selected={getFirstRoute(tab.url) == getFirstRoute(location.pathname)}>
             <ListItemPrefix>{tab.icon}</ListItemPrefix>
             {tab.text}
             {tab.badge && 
